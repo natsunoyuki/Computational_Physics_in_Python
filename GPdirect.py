f5c4a950fafe5e832d1633e118eb036bf4b3368a
natsunoyuki <33706936+natsunoyuki@users.noreply.github.com>
2018-05-06 13:01:33 +0900

    Update GPdirect.py

--- GPdirect.py
+++ GPdirect.py
@@ -2,14 +2,13 @@ from __future__ import division
 from scipy import *
 import matplotlib.pyplot as plt
 from scipy import linalg
-import time
 
-#This code shows how to calculate the correlation dimension of a nonlinear system based on the
-#equations proposed by:
-
-#Peter Grassberger and Itamar Procaccia (1983). "Measuring the Strangeness of Strange Attractors". Physica D: Nonlinear Phenomena. 9 (1‒2): 189‒208.
-
-#starttime=time.time()
+"""
+This code shows how to calculate the correlation dimension of a nonlinear system based on the
+equations proposed by:
+Peter Grassberger and Itamar Procaccia (1983). 
+"Measuring the Strangeness of Strange Attractors". Physica D: Nonlinear Phenomena. 9 (1‒2): 189‒208.
+"""
 
 def GPCdir(r,recsp):
     [nmax,dd]=shape(recsp)
@@ -26,14 +25,13 @@ def GPCdir(r,recsp):
         NN[i]=k
     return sum(NN)*2/nmax**2
 
+
 """
+Usage example:
 r=linspace(0.01,10,1000)
 C=zeros(len(r))
 for q in xrange(len(r)):
     C[q]=GPCdir(r[q],75,10,data)
 """
 
-#print GPCdir(1e2,v)
-
-#print time.time()-starttime
 
