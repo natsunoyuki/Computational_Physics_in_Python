854d93c1bf96751a75b8f747533fb92b4635df95
夏の雪 <33706936+natsunoyuki@users.noreply.github.com>
2019-07-11 21:35:50 +0900

    Add files via upload

---
+++ fouriertransform.py
@@ -0,0 +1,81 @@
+from __future__ import division
+import matplotlib.pyplot as plt
+from scipy import * 
+
+#returns next power of 2 as fft algorithms work fastest when the input data
+#length is a power of 2
+def nextpow2(n):
+    m_f = log2(n)
+    m_i = ceil(m_f)
+    return int32(2 ** m_i)
+
+#FFT function returns the frequency range up to Nyquist frequency
+#and absolute spectral magnitude.
+def FFT(y,t):
+    dt=t[2]-t[1]
+    Fs=1.0/dt
+    L=len(y)
+    Y=fft(y,L)*dt
+    #amp=abs(Y)/(L/2) #FFT single sided spectrum
+    amp=abs(Y) #or simply take the amplitude only?
+    T=L*dt #1/T=Fs/L
+    freq=arange(0,Fs/2,1/T) #list frequencies up to Nyquist frequency
+    #resize result vectors to match their lengths
+    if len(freq) < len(amp):
+        amp = amp[0:len(freq)]  # make both vectors the same size
+    elif len(amp) < len(freq):
+        freq = freq[0:len(amp)]
+    return freq,amp
+
+#CEPSTRUM calculates the ceptram of a time series. The cepstrum is basically
+#a fourier transform of a fourier transform and has units of time
+def CEPSTRUM(y,t):
+    dt=t[2]-t[1]
+    Fs=1.0/dt
+    L=len(y)
+    Y=fft(y,L)
+    amp=abs(Y)/(L/2) #FFT single sided spectrum
+    T=L*dt #1/T=Fs/L
+    freq=arange(0,Fs/2,1/T) #list frequencies up to Nyquist frequency
+    #C=real(ifft(log(abs(fft(y)))))
+    C=abs(ifft(log(abs(fft(y))**2)))**2
+    NumUniquePts=int(ceil((L+1)/2))
+    C=C[0:NumUniquePts]
+    q=arange(0,NumUniquePts,1)*dt
+    return q,C
+
+#these functions demonstrate explicitly the DFT algorithm, but should not 
+#be used because of the extremely slow speed. Faster algorithms use the fact
+#that FFT is symmetric.
+def DFT(x):
+    N=len(x)
+    X=zeros(N,'complex')
+    for k in range(0,N,1):
+        for n in range(0,N,1):
+            X[k]=X[k]+x[n]*exp(-1j*2*pi*k*n/N)
+    return X
+
+#inverse discrete transform
+def IDFT(X):
+    N=len(X)
+    x=zeros(N,'complex')
+    for n in range(0,N,1):
+        for k in range(0,N,1):
+            x[n]=x[n]+X[k]*exp(1j*2*pi*k*n/N)
+    return x/N
+
+#shifted inverse discrete transform
+def SIDFT(X,D):
+    N=len(X)
+    x=zeros(N,'complex')
+    for n in range(0,N,1):
+        for k in range(0,N,1):
+            x[n]=x[n]+exp(-1j*2*pi*k*D/N)*X[k]*exp(1j*2*pi*k*n/N)
+    return x/N
+
+#shifted fourier transform
+def SHIFTFT(X,D):
+    N=len(X)
+    for k in range(N):
+        X[k]=exp(-1j*2*pi*k*D/N)*X[k]
+    return X
