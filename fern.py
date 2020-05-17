182e17543a3513999d6f241c795a852b8e488ea0
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2020-05-17 12:27:49 +0900

    Update fern.py

--- fern.py
+++ fern.py
@@ -1,4 +1,4 @@
-from scipy import *
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy import random
 import time
@@ -6,11 +6,12 @@ import time
 #This code demonstrates the recursive method used to create a fractal. In this case
 #the fractal is the Barnsley fern.
 
-def fern(N):
-    #N is the total number of points to use. More points mean more minute detail but longer run times.
+def fern(N=10000):
+    #N is the total number of points to use. 
+    #More points mean more minute detail but longer run times.
     start = time.time()
-    x = zeros([N + 1, 2])
-    x[0, :] = array([0.5, 0.5])
+    x = np.zeros([N + 1, 2])
+    x[0, :] = np.array([0.5, 0.5])
     for k in range(N):
         r = random.random()
         if r <= 0.01:
@@ -27,12 +28,12 @@ def fern(N):
             x[k + 1, 1] = 0.26 * x[k, 0] + 0.24 * x[k, 1] + 0.44
 
     end = time.time() - start
-    print 'The total time elapsed is: '
-    print(end)
+    print('The total time elapsed is: {:.2f}s'.format(end))
     return x
 
 #example function call and plotting:
 x=fern(10000)
+plt.figure(figsize=[8,8])
 plt.plot(x[:,0], x[:,1], 'g.')
 plt.axis('equal')
 plt.show()
