60d5ec11523901eef9cada435d5d8411e3d46451
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2020-05-17 10:57:58 +0900

    Update mandelbrot.py

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,36 +1,38 @@
-from scipy import *
+import numpy as np
 import matplotlib.pyplot as plt
 import time
 
 #This code generates the Mandelbrot set!
-
-num=50
-N=1000 #size of grid
-start=time.time()
-X0=array([-2,2,-2,2])
-x0=X0[0]
-x1=X0[1]
-y0=X0[2]
-y1=X0[3]
-i=1j
-
-[x,y]=meshgrid(linspace(x0,x1,N),linspace(y0,y1,N)*i)
-z=x+y
-c=x+y
-Q=zeros([N,N])
-
-for j in range(num):
-    index=abs(z)<inf
-    Q[index]=Q[index]+1
-    #depending on the following conditions various fractal sets are produced.
-    #z=z**2+-0.835-0.2321*i
-    z=z**2+c #this one produces the classical mandelbrot set.
-    #z=z**3*exp(z)+0.33
-    #z=z**2*exp(z)+0.33
+def mandelbrot(num_iter=50,N=1000,X0=np.array([-2,2,-2,2]),fractal="Mandelbrot"):
+    start = time.time()
+    x0 = X0[0]
+    x1 = X0[1]
+    y0 = X0[2]
+    y1 = X0[3]
+    i = 1j
     
-end=time.time()-start
-print(end)
-plt.pcolormesh(linspace(x0,x1,N),linspace(y0,y1,N),Q)
-plt.axis('equal')
-plt.axes()
-plt.show()
+    [x,y] = np.meshgrid(np.linspace(x0,x1,N),np.linspace(y0,y1,N)*i)
+    z = x + y
+    c = x + y
+    Q = np.zeros([N,N])
+    
+    for j in range(num_iter):
+        index = np.abs(z) < np.inf
+        Q[index] = Q[index] + 1
+        #depending on the following conditions various fractal sets are produced.
+        if fractal == "Julia":
+            z = z ** 2 + -0.835 - 0.2321 * i
+        elif fractal == "Mandelbrot":
+            z = z ** 2 + c #this one produces the classical mandelbrot set.
+        #z=z**3*exp(z)+0.33
+        #z=z**2*exp(z)+0.33
+        
+    end = time.time() - start
+    print("Time elapsed: {:.3f}s".format(end))
+    
+    plt.figure(figsize=(8,8))
+    plt.pcolormesh(np.linspace(x0,x1,N),np.linspace(y0,y1,N),Q)
+    plt.axis('equal')
+    plt.show()
+
+mandelbrot(fractal="Julia")
