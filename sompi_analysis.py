56af2ebe54c336fb991d223ac4c3a8841e4a1bec
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2020-11-23 11:13:08 +0900

    Update sompi_analysis.py

--- sompi_analysis.py
+++ sompi_analysis.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import linalg
-from scipy import randn
+from numpy.random import randn
 from scipy import signal
 
 # This code demonstrates the Sompi algorithm used to decompose a signal into
