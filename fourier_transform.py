39c0a2897d349a1cb9814ec1ed7241f4c32a49f9
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2021-02-01 19:02:32 +0900

    Update fourier_transform.py

--- fourier_transform.py
+++ fourier_transform.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy import fft, ifft
+from scipy.fft import fft, ifft
 
 def nextpow2(n):
     """
@@ -20,10 +20,52 @@ def nextpow2(n):
     m_i = np.ceil(m_f)
     return np.int(2 ** m_i)
 
-def FFT(y,t):
+def dft(X):
     """
-    FFT function returns the frequency range up to Nyquist frequency
-    and absolute spectral magnitude.
+    Discrete Fourier Transform.
+
+    Inputs
+    ------
+    X: np.array
+        np.array of X values to be Fourier transformed. len(X) should be a power of 2
+
+    Returns
+    -------
+    x: np.array
+        DFT of X
+    """        
+    N = len(X)
+    x = np.zeros(N, 'complex')
+    K = np.arange(0, N, 1)
+    for n in range(0, N, 1):
+        x[n] = np.dot(X, np.exp(1j * 2 * np.pi * K * n / N))
+    return x
+
+def idft(X):
+    """
+    Inverse Discrete Fourier Transform. This is the inverse function of dft().
+
+    Inputs
+    ------
+    X: np.array
+        np.array of X values to be inverse Fourier transformed. len(X) should be a power of 2
+
+    Returns
+    -------
+    x: np.array
+        DFT of X
+    """
+    N = len(X)
+    x = np.zeros(N, 'complex')
+          
+    K = np.arange(0, N, 1)
+    for n in range(0, N, 1):
+        x[n] = np.dot(X, np.exp(-1j * 2 * np.pi * K * n / N))
+    return x / N
+
+def FFT(y, t):
+    """
+    FFT function returns the frequency range up to Nyquist frequency and absolute spectral magnitude.
     
     Inputs
     ------
@@ -39,22 +81,22 @@ def FFT(y,t):
     amp: np.array
         np.array of Fourier amplitudes
     """
-    dt=t[2]-t[1]
-    Fs=1.0/dt
-    L=len(y)
-    Y=fft(y,L)*dt #dt should mathematically be included in the result!
+    dt = t[2] - t[1]
+    Fs = 1.0 / dt
+    L = len(y)
+    Y = fft(y, L) * dt # dt should mathematically be included in the result!
     #amp=abs(Y)/(L/2) #FFT single sided spectrum
-    amp=abs(Y) #or simply take the amplitude only?
-    T=L*dt #1/T=Fs/L
-    freq=np.arange(0,Fs/2,1/T) #list frequencies up to Nyquist frequency
-    #resize result vectors to match their lengths
+    amp = abs(Y) #or simply take the amplitude only?
+    T = L * dt #1/T=Fs/L
+    freq = np.arange(0, Fs / 2, 1 / T) # list frequencies up to Nyquist frequency
+    # resize result vectors to match their lengths
     if len(freq) < len(amp):
         amp = amp[0:len(freq)]  # make both vectors the same size
     elif len(amp) < len(freq):
         freq = freq[0:len(amp)]
-    return freq,amp
+    return freq, amp
 
-def CEPSTRUM(y,t):
+def CEPSTRUM(y, t):
     """
     CEPSTRUM calculates the ceptram of a time series. The cepstrum is basically
     a fourier transform of a fourier transform and has units of time
@@ -73,45 +115,19 @@ def CEPSTRUM(y,t):
     C: np.array
         np.array of Cepstral amplitudes
     """
-    dt=t[2]-t[1]
-    #Fs=1.0/dt
-    L=len(y)
-    #Y=fft(y,L)
-    #amp=np.abs(Y)/(L/2) #FFT single sided spectrum
-    #T=L*dt #1/T=Fs/L
-    #freq=np.arange(0,Fs/2,1/T) #list frequencies up to Nyquist frequency
+    dt = t[2] - t[1]
+    #Fs = 1.0 / dt
+    L = len(y)
+    #Y = fft(y, L)
+    #amp = np.abs(Y)/(L/2) # FFT single sided spectrum
+    #T = L * dt #1/T=Fs/L
+    #freq = np.arange(0, Fs / 2, 1 / T) # list frequencies up to Nyquist frequency
     #C=real(ifft(log(abs(fft(y)))))
-    C=np.abs(ifft(np.log(np.abs(fft(y))**2)))**2
-    NumUniquePts=int(np.ceil((L+1)/2))
-    C=C[0:NumUniquePts]
-    q=np.arange(0,NumUniquePts,1)*dt
-    return q,C
-
-def DFT(x):
-    """
-    this function demonstrates explicitly the DFT algorithm, but should not 
-    be used because of the extremely slow speed. Faster algorithms use the fact
-    that FFT is symmetric.
-    """
-    N=len(x)
-    X=np.zeros(N,'complex')
-    for k in range(0,N,1):
-        for n in range(0,N,1):
-            X[k]=X[k]+x[n]*np.exp(-1j*2*np.pi*k*n/N)
-    return X
-
-def IDFT(X):
-    """
-    this function demonstrates explicitly the inverse DFT algorithm, but should not 
-    be used because of the extremely slow speed. Faster algorithms use the fact
-    that FFT is symmetric.
-    """
-    N=len(X)
-    x=np.zeros(N,'complex')
-    for n in range(0,N,1):
-        for k in range(0,N,1):
-            x[n]=x[n]+X[k]*np.exp(1j*2*np.pi*k*n/N)
-    return x/N
+    C = np.abs(ifft(np.log(np.abs(fft(y))**2)))**2
+    NumUniquePts = int(np.ceil((L + 1) / 2))
+    C = C[0:NumUniquePts]
+    q = np.arange(0, NumUniquePts, 1) * dt
+    return q, C
 
 def SIDFT(X,D):
     """
